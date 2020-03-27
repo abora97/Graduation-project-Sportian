44cc12bc080bfd4ba7766069f42ecf73e8c46465
Abora <https://github.com/abora97>
2020-03-27 14:28:18 +0200

    create splash screen

---
+++ app/src/main/java/com/example/graduationprojectsportian/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.graduationprojectsportian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        timerStart();
+        //set animation to imageView
+        ImageView imageView = findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        imageView.startAnimation(animation);
+    }
+
+    private void timerStart() {
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, IntroActivity.class));
+            finish();
+        }, 3000);
+    }
+}
