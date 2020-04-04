3e80657d249c7367365300d35991e877cdedb17a
AbdUllah <abdullah_99>
2020-04-04 02:27:08 +0200

    add dosts

--- app/src/main/java/com/example/graduationprojectsportian/IntroAppActivity.java
+++ app/src/main/java/com/example/graduationprojectsportian/IntroAppActivity.java
@@ -4,15 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class IntroAppActivity extends AppCompatActivity {
 
-    private ViewPager screenPager;
-  IntroViewPagerAdapter introViewPagerAdapter;
 
+    Button btnBack ,btnNext;
+
+    private TextView[] mDots;
+    LinearLayout linear;
+    private int mCurrentPage;
+
+
+    private ViewPager screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter;
 
 
     @Override
@@ -20,28 +32,91 @@ public class IntroAppActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_app);
 
+        btnBack   = findViewById(R.id.btn_back);
+        btnNext = findViewById(R.id.btn_next);
+        linear=findViewById(R.id.linear);
+
         //fill list
         List<ScreenItem> mList = new ArrayList<>();
-        mList.add(new ScreenItem("  unpopular sports","         In this application \n" +
+        mList.add(new ScreenItem("  unpopular sports", "         In this application \n" +
                 "youth can read the latest news\n" +
-                "    about un popular sports  \n ",R.drawable.screen_start));
-        mList.add(new ScreenItem("hello","     help youth to find the nearest place\n" +
+                "    about un popular sports  \n ", R.drawable.screen_start));
+        mList.add(new ScreenItem("hello", "     help youth to find the nearest place\n" +
                 "   they want to play their sports through \n" +
                 "     the youth sports clubs which follow \n" +
-                "       the ministry of youth and sports \n",R.drawable.screen_sec));
-
-
+                "       the ministry of youth and sports \n", R.drawable.screen_sec));
 
 
         //setup viewPager
 
-        screenPager =findViewById(R.id.screen_viewpager);
-        introViewPagerAdapter=new IntroViewPagerAdapter(this,mList);
+        screenPager = findViewById(R.id.screen_viewpager);
+        introViewPagerAdapter = new IntroViewPagerAdapter(this, mList);
         screenPager.setAdapter(introViewPagerAdapter);
 
 
+        addDotsIndicator(0);
+        screenPager.addOnPageChangeListener(viewListener);
 
 
 
     }
+
+    private void addDotsIndicator(int position) {
+        mDots = new TextView[2];
+        linear.removeAllViews();
+        for (int i = 0; i < mDots.length; i++) {
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.transparentWhite));
+
+            linear.addView(mDots[i]);
+        }
+
+        if (mDots.length > 0) {
+            mDots[position].setTextColor(getResources().getColor(R.color.white));
+        }
+
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotsIndicator(position);
+            mCurrentPage = position;
+            if (position == 0) {
+                btnNext.setEnabled(true);
+                btnBack.setEnabled(false);
+                btnBack.setVisibility(View.INVISIBLE);
+
+                btnNext.setText("Next");
+                btnBack.setText("");
+            } else if (position == mDots.length - 1) {
+                btnNext.setEnabled(true);
+                btnBack.setEnabled(true);
+                btnBack.setVisibility(View.VISIBLE);
+
+                btnNext.setText("Finish");
+                btnBack.setText("Back");
+            } else {
+                btnNext.setEnabled(true);
+                btnBack.setEnabled(true);
+                btnBack.setVisibility(View.VISIBLE);
+
+                btnNext.setText("Next");
+                btnBack.setText("Back");
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
 }
