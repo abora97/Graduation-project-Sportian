67e63b681e85a6f17f37821ab390ae9911c76385
Abora <https://github.com/abora97>
2020-07-04 02:40:33 +0200

    fix permissions issue

--- app/src/main/java/com/example/graduationprojectsportian/ui/activity/SplashActivity.java
+++ app/src/main/java/com/example/graduationprojectsportian/ui/activity/SplashActivity.java
@@ -49,6 +49,8 @@ public class SplashActivity extends AppCompatActivity {
             case MAKE_CALL_PERMISSION_REQUEST_CODE :
                 if (grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     timerStart();
+                }else {
+                    finish();
                 }
                 return;
         }
